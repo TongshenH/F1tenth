fd9b6c13a2e7151907e06fef8f5377f373eaa50f
Tongshen <thou@clemson.edu>
2023-10-25 12:19:05 -0400

    Implemented Auto Emergency Brake

---
+++ f110_simulator/node/auto_brake.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+import rospy
+import math
+
+
+from sensor_msgs.msg import LaserScan
+from ackermann_msgs.msg import AckermannDriveStamped
+from std_msgs.msg import Bool
+from nav_msgs.msg import Odometry
+import squaternion as quat
+import numpy as np
+
+
+class Safety(object):
+    """
+    The class that handles emergency braking.
+    """
+
+    def __init__(self):
+        rospy.init_node(name="Safety", anonymous=True)
+        
+        # Init vehicle parameters
+        self.ackerman = AckermannDriveStamped()
+        self.brake = Bool()
+        self.x = 0
+        self.y = 0
+        self.yaw = 0
+        self.threshold = 3
+
+        # Publish AckermanDriveStamp message
+        self.brake_act_pub = rospy.Publisher("/brake", AckermannDriveStamped, queue_size=1)
+        # Publish bool message
+        self.brake_pub = rospy.Publisher('/brake_bool', Bool, queue_size=1)
+        # Subscribe the odom message
+        self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_callback)
+        # Subscribe the laser scan message
+        self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.scan_callback)
+
+    def odom_callback(self, odom_msg):
+        # update current speed
+        self.x = odom_msg.pose.pose.position.x
+        self.y = odom_msg.pose.pose.position.y
+        self.vel = odom_msg.twist.twist.linear.x
+        q = odom_msg.pose.pose.orientation
+        q2 = quat.Quaternion(q.w, q.x, q.y, q.z)
+        self.yaw = q2.to_euler()[2]
+
+    def scan_callback(self, scan_msg):
+
+        for i in range(len(scan_msg.ranges)):
+            obs_dis = scan_msg.ranges[i]
+
+            if not math.isinf(obs_dis) and not math.isnan(obs_dis):
+                # Calculate Time-to-Collision
+                angle_increment = scan_msg.angle_increment
+                ttc_denominator = max(0.0, self.vel*math.cos(i*angle_increment-math.pi))
+
+                if ttc_denominator != 0:
+                    ttc = obs_dis / ttc_denominator
+                else:
+                    ttc = float('inf')
+                
+                # Reverse motion
+                if self.vel < 0 and ttc < abs(self.vel) / 8.26 + 0.12:
+                    self.brake = True
+                    self.ackerman.drive.speed = 0
+                    self.brake_act_pub.publish(self.ackerman)
+                    self.brake_pub.publish(self.brake)
+
+                # Forward motion
+                elif self.vel > 0 and ttc < self.vel * 0.05:
+                    self.brake = True
+                    self.ackerman.drive.speed = 0
+                    # Uncomment the line below for tuning minimum TTC
+                    print("velocity:", self.vel)
+                    print("TTC{}: {}".format(i, ttc))
+                    self.brake_act_pub.publish(self.ackerman)
+                    self.brake_pub.publish(self.brake)
+
+
+def main():
+    sn = Safety()
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
