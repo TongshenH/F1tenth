624125890f19eeabd2506b79d5a5cb62af2b4b46
Tongshen <thou@clemson.edu>
2023-12-03 14:14:21 -0500

    added the auto emegency brake to the local path planning

--- f110_simulator/node/local_planning_path_tracking.py
+++ f110_simulator/node/local_planning_path_tracking.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import os
+import math
 from collections import namedtuple
 
 current = os.path.dirname(os.path.realpath(__file__))
@@ -12,10 +13,10 @@ print(sys.path)
 import rospy
 import subprocess
 import numpy as np
-from std_msgs.msg import String
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import String, Bool
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Odometry, Path
-# from novatel_oem7_msgs.msg import HEADING2
 from ackermann_msgs.msg import AckermannDriveStamped
 import squaternion as quat
 
@@ -23,7 +24,6 @@ from src.State import State
 from src.stanley_controller2 import StanleyController
 from src.CubicSpline.cubic_spline_planner import calc_spline_course
 from src.frenet_path_planning import FrenetPathPlanning
-# from RTK_motion_planning.msg import BBox3d, Detection
 from src.Path import FrenetPath
 from src.Visualization import visualization
 from src.params import *
@@ -47,8 +47,15 @@ class PathTrackingNode:
 
         # Init the vehicle state
         self.actual_steering = 0
+        self.vel = None
         self.state = State(self.traj_d, x=0, y=0, yaw=0, v=0)
+        
+        # Init Time to Collision (TTC) check parameters
         self.obs = []
+        self.beams = None
+        self.brake = Bool()
+        self.threshold = 0.1
+        
 
         # Init the Stanley controller
         self.stanley_controller = StanleyController(self.traj_d, K_S, L_W)
@@ -65,8 +72,11 @@ class PathTrackingNode:
         self.local_path_pub = None
         self.best_local_pub = rospy.Publisher('/best_local_path', Path, queue_size=10)
 
-        # Init subscribers to retrieve obstacle information
-        # self.obs_sub = rospy.Subscriber("/obstacle2", Detection, self.obs_callback)
+        # Publish bool message
+        self.brake_pub = rospy.Publisher('/brake_bool', Bool, queue_size=10)
+
+        # Init lidar beams scan subscriber
+        self.lidar_sub = rospy.Subscriber('/scan', LaserScan, self.scan_callback)
 
         # Init the actual steering angle subscriber
         self.actual_steering_sub = rospy.Subscriber(
@@ -75,7 +85,7 @@ class PathTrackingNode:
 
         # Init instances for publishing Ackermann msg
         self.ackermann_msg = AckermannDriveStamped()
-        self.ackermman_pub = rospy.Publisher(
+        self.ackermann_pub = rospy.Publisher(
             '/drive', AckermannDriveStamped, queue_size=1)
 
         # Init subscribers to retrieve simulation status in the simulation mode
@@ -137,10 +147,59 @@ class PathTrackingNode:
         q2 = quat.Quaternion(q.w, q.x, q.y, q.z)
         yaw = q2.to_euler()[2]
         # Get the vel of the vehicle
-        # vel = data.twist.twist.linear.x
-        vel = 4.0
-        self.state.update(x, y, yaw, vel, self.actual_steering)
+        # self.vel = data.twist.twist.linear.x
+        self.vel = 4
+        self.state.update(x, y, yaw, self.vel, self.actual_steering)
         self.state.cart2frenet()
+
+    def scan_callback(self, scan_msg):
+        """Get the lidar scan topic
+
+        scan_msg: scan
+            The list of each beam's distance to the obstacle
+        """
+        self.beams = []
+        for i in range(len(scan_msg.ranges)):
+            obs_dis = scan_msg.ranges[i]
+            self.angle_increment = scan_msg.angle_increment
+            self.beams.append(obs_dis)
+        # Time to collision check
+        self.ttc_check()
+
+    def ttc_check(self):
+        """Check the TTC
+        Returns:
+            binary: brake or not
+        """
+        for i, obs_dis in enumerate(self.beams):
+            if not math.isinf(obs_dis) and not math.isnan(obs_dis):
+
+                # Calculate Time-to-Collision
+                angle_increment = self.angle_increment
+                ttc_denominator = max(0.0, self.vel*
+                                      math.cos(i*angle_increment-math.pi))
+
+                if ttc_denominator != 0:
+                    ttc = obs_dis / ttc_denominator
+                else:
+                    ttc = float('inf')
+                
+                # Reverse motion
+                if self.vel < 0 and ttc < self.threshold:
+                    self.brake = True
+                    self.ackermann_msg.drive.speed = 0
+                    self.ackermann_pub.publish(self.ackermann_msg)
+                    self.brake_pub.publish(self.brake)
+
+                # Forward motion
+                elif self.vel >= 0 and ttc < self.threshold:
+                    self.brake = True
+                    self.ackermann_msg.drive.speed = 0
+                    # Uncomment the line below for tuning minimum TTC
+                    print("velocity:", self.vel)
+                    print("TTC{}: {}".format(i, ttc))
+                    self.ackermann_pub.publish(self.ackermann_msg)
+                    self.brake_pub.publish(self.brake)
 
     def load_path(self, filepath):
         """Load the predefined path
@@ -167,7 +226,7 @@ class PathTrackingNode:
         target_idx, _ = self.stanley_controller.calc_target_index(self.state)
 
         while not rospy.is_shutdown():
-            
+  
             local_paths = []
             path, det_range, num_obb, fplist = self.frenet_optimal_planner.frenet_optimal_planning(self.state, self.obs)
             
@@ -214,22 +273,22 @@ class PathTrackingNode:
             if np.hypot(path.x[1] - self.traj_d.cx[-1], path.y[1] - self.traj_d.cy[-1]) <= 2.0:
                 self.ackermann_msg.drive.speed = 0
                 self.ackermann_msg.drive.steering_angle = 0
-                self.ackermman_pub.publish(self.ackermann_msg)
+                self.ackermann_pub.publish(self.ackermann_msg)
                 rospy.loginfo('the target index is {}'.format(target_idx))
                 rospy.loginfo("reach the destination!")
                 break
 
-            # publish the steering angle and libnear speed
+            # publish the steering angle and linear speed
             self.ackermann_msg.drive.speed = path.s_d[-1]
             self.ackermann_msg.drive.steering_angle = -1 * (steering_angle)
-            self.ackermman_pub.publish(self.ackermann_msg)
+            self.ackermann_pub.publish(self.ackermann_msg)
             self.rate.sleep()
 
     def shutdown(self):
         rospy.loginfo("Shutting down!")
         self.ackermann_msg.drive.speed = 0
         self.ackermann_msg.drive.steering_angle = 0
-        self.ackermman_pub.publish(self.ackermann_msg)
+        self.ackermann_pub.publish(self.ackermann_msg)
 
 
 if __name__ == '__main__':
